a16acd65914324f82c184dd10b1a09aa50d7df88
kyoowook song <kyoows@gmaile.com>
2025-01-30 23:31:47 +0900

    1008

---
+++ boj/1000/1008/1008.c
@@ -0,0 +1,13 @@
+/*
+ * 1008 - A/B
+ * 89342610 helloneo 1008 맞았습니다!! 1112 0 C99 
+ */
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    double a, b;
+    scanf("%lf%lf", &a, &b);
+    printf("%.9lf\n", a/b);
+    return 0;
+}
